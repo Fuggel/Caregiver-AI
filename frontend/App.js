053d0489c795a4975d07124b00c2b5ec060f7d49
Fuggel <furkanserdarceylan@gmail.com>
2023-12-21 00:41:01 +0100

    style(frontend): use dark bar style for ios

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
@@ -12,6 +12,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="default" />
       <ChatProvider>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
